3a8dcb99e45269d6dd2cfcd2cfedf8ffeb0a53c4
thor56 <ranjithkmr598@gmail.com>
2022-05-30 14:48:28 +0530

    Column sorting and Weekdays sorting

--- app.py
+++ app.py
@@ -36,7 +36,15 @@ def process(df, filename1):
     df['Name, State,Pin'] = df['Name'] + ", " + df['State'] + ", " +  df['Pin'] 
     df['Location'] = df['Location'].map(location)
     print(df)
-    output = df.to_csv(index=False)
+    df2 = pd.DataFrame()
+    df2.insert(0,"Name, State,Pin",df['Name, State,Pin'],True)
+    df2.insert(1,"Phone Number",df['Phone Number'],True)
+    df2.insert(2,"Location",df['Location'],True)
+    df2.insert(3,"Full Address",df['Full Address'],True)
+    df2.insert(4,"Opening hours",df['Opening hours'],True)
+    print(df2)
+
+    output = df2.to_csv(index=False)
     os.remove(filename1)
     return Response(
         output ,
@@ -46,8 +54,21 @@ def process(df, filename1):
 
 
 def stemmingWords(sentence):
-    dict = {'monday': 'Mon' , 'tuesday' : 'Tue', 'wednesday': 'Wed','thursday': 'Thu',  'friday': 'Fri','saturday': 'Sat', 'sunday': 'Sun'  }
-    return " ".join([dict.get(w,w) for w in sentence.replace(":"," : ").replace("["," ").replace("]"," ").replace(",","<br>").lower().split()])
+    dict_ = {'monday': 'Mon' , 'tuesday' : 'Tue', 'wednesday': 'Wed','thursday': 'Thu',  'friday': 'Fri','saturday': 'Sat', 'sunday': 'Sun'  }
+    str2 =  " ".join([dict_.get(w,w) for w in sentence.replace(":"," : ").replace("["," ").replace("]"," ").replace(",","<br>").lower().split()])
+    lst2 = str2.split(" <br> ")
+    dict2 = dict()
+    for x in lst2:
+        dict2[x[:3]] = x
+
+    l = [] 
+    for x in dict_: 
+        l.append(dict2[dict_[x]])
+
+    return " <br> ".join(l)
+
+
+
 
 def separatelocality1(sentence):
     return sentence.replace(", ",",").split(",")[0]
